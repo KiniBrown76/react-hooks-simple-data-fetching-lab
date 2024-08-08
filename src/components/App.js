c777d4f5db3de318d35299b571cb638dd01a7dc1
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-08-08 18:31:17 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [imgUrl, setImgUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        setImgUrl(data.message);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImage();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading image...</p>
+      ) : (
+        <img src={imgUrl} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
